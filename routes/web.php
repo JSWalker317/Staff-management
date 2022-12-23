a37c6875b0fe286c1cd2f8093cee214d698bd5b1
root <root@jswalker.localdomain>
2022-12-23 10:59:33 +0700

    config routes permit login

--- routes/web.php
+++ routes/web.php
@@ -24,54 +24,55 @@ Route::get('/login', [AuthController::class, 'getLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'postLogin'])->name('postLogin');
 Route::post('/register', [AuthController::class, 'register'])->name('register');
 
-// Product page
-Route::controller(ProductController::class)
-->prefix('product')
-->as('product.')
-->group(function() {
-    Route::get('/', 'index')->name('products');
-    // Route::get('/', 'index')->name('products');
-    Route::get('/fetchData', 'fetchData')->name('fetchData');
-    Route::get('/getViewPost', 'getViewPost')->name('getViewPost');
-    // Route::post('product/postProduct', 'postProduct');
-    Route::post('/storeProduct', 'storeProduct')->name('store');
-    Route::get('/{id}', 'show')->name('show');
-    Route::get('/delete/{id}', 'delete')->name('delete');
-});
-
- // Customer page
- Route::controller(CustomerController::class)
-->prefix('customer')
-->as('customer.')
- ->group(function() {
-    Route::get('/', 'index')->name('customers');
-    Route::get('/fetchData', 'fetchData')->name('fetchData');
-    Route::get('/export', 'export')->name('export');
-
-    Route::post('/postCustomer', 'postCustomer')->name('postCustomer');
-    Route::post('/import','import')->name('import');
-    Route::get('/{customer_id}', 'show')->name('show');
-});
-
-// User page
-Route::controller(UserController::class)
-->prefix('user')
-->as('user.')
-->group(function() {
-    Route::get('/', 'index')->name('users');
-    Route::get('/fetchData', 'fetchData')->name('fetchData');
-    Route::post('/postUser', 'postUser')->name('postUser');
-
-    Route::get('/delete/{id}', 'delete')->name('delete');
-    Route::get('/{id}', 'show')->name('show');
-    Route::get('/status/{id}','setStatus')->name('setStatus');
-});
-
 // private
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
-   
+
+    // Product page
+    Route::controller(ProductController::class)
+    ->prefix('product')
+    ->as('product.')
+    ->group(function() {
+        Route::get('/', 'index')->name('products');
+        // Route::get('/', 'index')->name('products');
+        Route::get('/fetchData', 'fetchData')->name('fetchData');
+        Route::get('/getViewPost', 'getViewPost')->name('getViewPost');
+        // Route::post('product/postProduct', 'postProduct');
+        Route::post('/storeProduct', 'storeProduct')->name('store');
+        Route::get('/{id}', 'show')->name('show');
+        Route::get('/delete/{id}', 'delete')->name('delete');
+    });
+
+ 
+    // Customer page
+    Route::controller(CustomerController::class)
+    ->prefix('customer')
+    ->as('customer.')
+    ->group(function() {
+        Route::get('/', 'index')->name('customers');
+        Route::get('/fetchData', 'fetchData')->name('fetchData');
+        Route::get('/export', 'export')->name('export');
+    
+        Route::post('/postCustomer', 'postCustomer')->name('postCustomer');
+        Route::post('/import','import')->name('import');
+        Route::get('/{customer_id}', 'show')->name('show');
+    });
+ 
+    // User page
+    Route::controller(UserController::class)
+    ->prefix('user')
+    ->as('user.')
+    ->group(function() {
+        Route::get('/', 'index')->name('users');
+        Route::get('/fetchData', 'fetchData')->name('fetchData');
+        Route::post('/postUser', 'postUser')->name('postUser');
+    
+        Route::get('/delete/{id}', 'delete')->name('delete');
+        Route::get('/{id}', 'show')->name('show');
+        Route::get('/status/{id}','setStatus')->name('setStatus');
+    });
+ 
 
     // Route::get('/product', [ProductController::class, 'index'])->name('products');
     // Route::get('/customer', [CustomerController::class, 'index'])->name('customers');
